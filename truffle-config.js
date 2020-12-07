4ec24841e2ae7d0debab3b046f254bf00c033c17
Joel Reis <joelhreis@gmail.com>
2020-12-07 15:54:37 -0500

    Added rinkeby deployment name to use live ERC20 contracts (USDC : cUSDC)

--- truffle-config.js
+++ truffle-config.js
@@ -1,20 +1,27 @@
-const path = require("path");
+const path = require("path")
 
 module.exports = {
   compilers: {
     solc: {
-      version: "^0.7.0"
-    }
+      version: "^0.7.0",
+    },
   },
 
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "app/src/contracts"),
   networks: {
-    develop: { // default with truffle unbox is 7545, but we can use develop to test changes, ex. truffle migrate --network develop
+    develop: {
+      // default with truffle unbox is 7545, but we can use develop to test changes, ex. truffle migrate --network develop
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*"
-    }
-  }
-};
+      network_id: "*",
+    },
+    // Since I deploy rinkeby from local blockchain instance, this is setup the same as develop, but requires a unique name for identifying trading pairs
+    rinkeby: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+    },
+  },
+}
